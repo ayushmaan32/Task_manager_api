ea78190d491579dd8b81c5de5b5d7a87e8026ab2
ayushmaan32 <ayushmaanroy.12@gmail.com>
2024-05-19 19:14:26 +0530

    changed in port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 const db = require("./config/mongoose");
 
